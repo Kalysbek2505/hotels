50e387963e183dcae439884543d99bb03f15b928
Kalys2505 <zarypbekov.kalys@gmail.com>
2022-08-15 14:37:14 +0600

    .

--- hotel/models.py
+++ hotel/models.py
@@ -21,7 +21,7 @@ class Comment(models.Model):
     body = models.TextField()
     created_at = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
-    post = models.ForeignKey(Room, related_name='comments', on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name='comments', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Comment{self.user.username} -> {self.post.title} [{self.created_at}]"
