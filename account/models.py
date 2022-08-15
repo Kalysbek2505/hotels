895d207746376275234afe8211e9fad1724587cc
Kalys2505 <zarypbekov.kalys@gmail.com>
2022-08-15 16:29:51 +0600

    .

--- account/models.py
+++ account/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.forms import CharField
+from django.contrib.auth.models import User
 
 
 class UserManager(BaseUserManager):
