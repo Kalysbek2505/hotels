8530effcc93034bc57979b64d728d435cccd924a
Beknaz <choybekov.beknaz@gmail.com>
2022-08-15 12:27:25 +0600

    .

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Room, Comment, Like
 
-class ProductSerializer(serializers.ModelSerializer):
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = '__all__'
--- hotel/views.py
+++ hotel/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework import mixins, filters
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.decorators import api_view, action
-from .models import Product, Category, Comment, Like, Rating
-from .serializers import ProductSerializer, CategorySerializer, CommentSerializer
+from .models import Room, Comment, Like, Rating
+from .serializers import ProductSerializer, CommentSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .permissions import IsAuthor
@@ -11,8 +11,8 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from rest_framework import generics
 
-class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+class RoomViewSet(ModelViewSet):
+    queryset = Room.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [filters.OrderingFilter]
@@ -61,18 +61,18 @@ class CommentViewSet(mixins.CreateModelMixin,
 @api_view(["GET"])
 def toggle_like(request, p_id):
     user = request.user
-    product = get_object_or_404(Product, id=p_id)
+    room = get_object_or_404(Room, id=p_id)
     
-    if Like.objects.filter(user=user, product=product).exists():
-        Like.objects.filter(user=user, product=product).delete()
+    if Like.objects.filter(user=user, room=room).exists():
+        Like.objects.filter(user=user, room=room).delete()
     else:
-        Like.objects.create(user=user, product=product)
+        Like.objects.create(user=user, room=room)
     return Response("Like toggled", 200)
 
 @api_view(["POST"])
 def add_rating(request, p_id):
     user = request.user
-    product = get_object_or_404(Product, id=p_id)
+    room = get_object_or_404(Room, id=p_id)
     value = request.POST.get("value")
 
     if not user.is_authenticated:
@@ -81,10 +81,10 @@ def add_rating(request, p_id):
     if not value:
         raise ValueError("value is required")
     
-    if Rating.objects.filter(user=user, product=product).exists():
-        rating = Rating.objects.get(user=user, product=product)
+    if Rating.objects.filter(user=user, room=room).exists():
+        rating = Rating.objects.get(user=user, room=room)
         rating.value=value
         rating.save()
     else:
-        Rating.objects.create(user=user, product=product, value=value)
+        Rating.objects.create(user=user, room=room, value=value)
     return Response("rating created", 201)
